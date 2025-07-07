ace8b8109596d571d21c840a3a7f1022e614aca2
Hitheshpp <109617723+Hitheshpp@users.noreply.github.com>
2025-07-07 10:24:30 +0530

    Update face_model.py

--- face_model.py
+++ face_model.py
@@ -1,12 +1,20 @@
 # face_model.py
 
 from insightface.app import FaceAnalysis
+import threading
 
 _model_instance = None
+_model_lock = threading.Lock()
 
 def get_face_model():
     global _model_instance
+
     if _model_instance is None:
-        _model_instance = FaceAnalysis(name='buffalo_l', providers=['CPUExecutionProvider'])
-        _model_instance.prepare(ctx_id=0)
+        with _model_lock:
+            if _model_instance is None:  # Double-checked locking
+                print("[INFO] Loading InsightFace model...")
+                _model_instance = FaceAnalysis(name='buffalo_l', providers=['CPUExecutionProvider'])
+                _model_instance.prepare(ctx_id=0)
+                print("[INFO] InsightFace model loaded.")
+    
     return _model_instance
